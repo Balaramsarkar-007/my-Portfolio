296e7a9083a7763db430dc475da3fcbe657f14dd
Balaram Sarkar <balaramsarkar.ramnagar@gmail.com>
2024-04-17 11:27:47 +0530

    Merge branch 'main' of https://github.com/Balaramsarkar-007/my-portfolio

--- app.js
+++ app.js
@@ -71,5 +71,28 @@ particlesJS("particles-js", {
   };
   requestAnimationFrame(update);
 
+<<<<<<< HEAD
 
  
+=======
+  //form validation 
+
+  (() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+>>>>>>> db52fb3 (Merge branch 'main' of https://github.com/Balaramsarkar-007/my-portfolio)
